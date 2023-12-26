398f975c41588d96b7ba9133ca270a926fafccaa
Issay Tieu <issaytieu@Issays-MacBook-Pro.local>
2023-12-26 17:28:53 -0500

    Created processData class and created subsequent functions to filter out x and y values

--- process_data.py
+++ process_data.py
@@ -1,10 +1,5 @@
 from stock_class import stock
-
-import os
-
 from datetime import datetime
-
-import pandas_datareader.data as web
 
 #New Concept:
 #Use pandas data reader to extract data from alphavantage api
@@ -13,26 +8,87 @@ import pandas_datareader.data as web
 #Use flask to create the website.
 # integrate your html with flask by passing the labels, values to the javascript.
 
-#Required: symbol is a valid ticker symbol
-def getIntradayData(symbol) :
-    #const chart_data = await new stock_data('TIME_SERIES_INTRADAY', symbol).request_data();
-    #const lastRefreshedDate = new Date(chart_data["Meta Data"]["3. Last Refreshed"]);
-    #var startingDate = new Date(num_weekdays_ago_date(lastRefreshedDate, 1));
-    #console.log(startingDate.getDate());
-    #const timeSeries = chart_data["Time Series (5min)"];
+class processData :
+    def __init__(self, stock, numdays = 0, dtf = "%Y-%m-%d %H:%M:%S", typePrice = "4. close"):
+        self.stock = stock
+        self.numdays = numdays
+        self.dtf = dtf
+        self.typePrice = typePrice
 
-    #// Find/put time/price data in seperate arrays in chronological order.
-    #let timestamps = Object.keys(timeSeries).map(timestamp => new Date(timestamp));    
-    #let closePrices = Object.values(timeSeries).map(entry => parseFloat(entry["4. close"]));
-
-    #// Ensure that timeSeries and timestamps only contain data from one day.
-    #const start_date_index = starting_date_index(timestamps, startingDate);
-    #timestamps = timestamps.slice(0, start_date_index).reverse();
-    #closePrices = closePrices.slice(0, start_date_index).reverse();
-
-    data = web.DataReader(symbol, "av-intraday", start=datetime(2017, 2, 9),
-                          end=datetime(2017, 5, 24),
-                          api_key=os.getenv('ALPHAVANTAGE_API_KEY'))
+    def getChartData(self) :
+        chart_data = self.stock.get_data()
+        # Extract start date of the data set for reference.
+        last_refreshed_date = chart_data['Meta Data']['3. Last Refreshed'].split()[0]
         
-}
+        # Put timestamps and closePrices (x/y values) into separate arrays.
+        timestamps = []
+        closePrices = []
+        keyIdentifier = list(chart_data.keys())[1] # Ex. "Time Series (5min)"
+        currentNumDays = 1
+        tempCurrDate = last_refreshed_date
+        for time, price in chart_data[keyIdentifier].items() :
+            date = time.split()[0]
+            if (date != tempCurrDate) :
+                tempCurrDate = date
+                currentNumDays = currentNumDays + 1
 
+            if (currentNumDays <= self.numdays) :
+                # Convert string to datetime object
+                timestamps.insert(0,datetime.strptime(time, self.dtf))
+                closePrices.insert(0, price[self.typePrice])
+            else :
+                break
+        return [timestamps, closePrices]
+    def getYTDChartData(self) :
+        chart_data = self.stock.get_data()
+        # Extract start date of the data set for reference.
+        last_refreshed_date = chart_data['Meta Data']['3. Last Refreshed'].split()[0]
+        
+        # Put timestamps and closePrices (x/y values) into separate arrays.
+        timestamps = []
+        closePrices = []
+        keyIdentifier = list(chart_data.keys())[1] # Ex. "Time Series (5min)"
+        for time, price in chart_data[keyIdentifier].items() :
+            date = time.split()[0]
+            if (datetime.strptime(date, "%Y-%m-%d").year() == 
+                datetime.strptime(last_refreshed_date, "%Y-%m-%d").year()) :
+                # Convert string to datetime object
+                timestamps.insert(0,datetime.strptime(time, "%Y-%m-%d"))
+                closePrices.insert(0, price[self.typePrice])
+            else :
+                break
+        return [timestamps, closePrices]
+
+# Required: symbol is a valid ticker symbol
+# Returns an array containing [timestamps, closePrices].
+def getIntradayData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_INTRADAY')
+    return processData(currStock, 1).getChartData()
+
+def getWeeklyData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_INTRADAY', '5min', 'full')
+    return processData(currStock, 5).getChartData()
+
+def getMonthlyData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_DAILY')
+    return processData(currStock, 21, "%Y-%m-%d").getChartData()
+
+def getYTDData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_DAILY', '5min', 'full')
+    return processData(currStock, dtf = "%Y-%m-%d").getYTDChartData()
+
+def getOneYearData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_DAILY', '5min', 'full')
+    return processData(currStock, 260, "%Y-%m-%d").getChartData()
+
+def getFiveYearData(symbol) :
+    currStock = stock(symbol, 'TIME_SERIES_WEEKLY_ADJUSTED')
+    return processData(currStock, 262, "%Y-%m-%d", "5. adjusted close").getChartData()
+
+# symbol = 'AAPL'
+# data = getFiveYearData(symbol)
+# xvalues = data[0]
+# yvalues = data[1]
+# print(len(xvalues))
+# for i in range(len(xvalues)) :
+#     print(xvalues[i].strftime("%Y-%m-%d") + " : " + yvalues[i])
