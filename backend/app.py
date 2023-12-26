0d377a0ef175a3c9cce8cffdc4dbae8343eb0249
Issay Tieu <issaytieu@Issays-MacBook-Pro.local>
2023-12-26 18:11:49 -0500

    updated directory

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-#from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
 load_dotenv()
-
 # Access the API key
 api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
 
-#print(api_key)
+app = Flask(__name__)
