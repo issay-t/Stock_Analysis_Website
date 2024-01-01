f35d4924bf77dd87702868e8398fcbadd6edea06
Issay Tieu <issaytieu@Issays-MacBook-Pro.local>
2024-01-01 17:56:56 -0500

    Finish all last touches, need to add basic titles

--- static/stock_info.js
+++ static/stock_info.js
@@ -12,12 +12,6 @@ function getStockData(lengthTime) {
             return response.json();
         })
         .then(data => {
-            //Convert timestamps to js date objects and adjust for eastern time.
-            data['timestamps'] = data['timestamps'].map(timestamp => new Date(timestamp));
-            data['timestamps'].forEach((timestamp, index) => {
-                const currentHours = timestamp.getHours();
-                timestamps[index] = new Date(timestamp.setHours(currentHours + 5));
-            });
             return data;
         })
         .catch(error => {
@@ -26,111 +20,409 @@ function getStockData(lengthTime) {
         });
 }
 
-// Function to display intraday line graph of stock data:
+// Class that assists in creating functions to display different charts.
+class create_chart {
+    constructor(lengthTime, options){
+        this.lengthTime = lengthTime;
+        this.options = options;
+    }
+
+    // Function to display the chart.
+    async displayChart(){
+        try {
+            const chart_data = await getStockData(this.lengthTime);
+            // console.log(chart_data['timestamps']);
+            // console.log(chart_data['closePrices']);
+            var timestamps = chart_data['timestamps'].map(timestamp => new Date(timestamp));
+            // Adjust hours to match UTD.
+            timestamps.forEach((timestamp, index) => {
+                const currentHours = timestamp.getHours();
+                timestamps[index] = new Date(timestamp.setHours(currentHours + 5));
+            });
+            //timestamps = chart_data['timestamps'];
+            var closePrices = chart_data['closePrices'];
+            //console.log("Up to here.");
+            // for (var i = 0; i < closePrices.length; i++) {
+            //     console.log(timestamps[i] + ': ' + closePrices[i]);
+            // }
+        
+            // Create and display chart.
+            const ctx = document.getElementById("StockChart").getContext("2d");
+    
+            // Destroy old chart if it exists:
+            var pos = $(document).scrollTop();
+            if (window.myChart) {
+                window.myChart.destroy();
+            }
+            var windowSettings = {
+                type: "line",
+                data: {
+                    labels: timestamps,
+                    datasets: [{
+                        label: "Close Prices",
+                        data: closePrices,
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1,
+                        fill: false
+                    }]
+                },
+                options: {}
+            }
+            windowSettings['options'] = this.options;
+            window.myChart = new Chart(ctx, windowSettings);
+            $(document).scrollTop(pos);
+        } catch (error) {
+            console.error('Error displaying chart:', error);
+        }
+    }
+}
+
+// // Function to display intraday line graph of stock data:
+// async function display_intraday_chart(){
+//     try {
+//         const chart_data = await getStockData('intraday');
+//         //console.log(chart_data)
+//         timestamps = chart_data['timestamps'].map(timestamp => new Date(timestamp));
+//         // Adjust hours to match UTD.
+//         timestamps.forEach((timestamp, index) => {
+//             const currentHours = timestamp.getHours();
+//             timestamps[index] = new Date(timestamp.setHours(currentHours + 5));
+//         });
+//         //timestamps = chart_data['timestamps'];
+//         closePrices = chart_data['closePrices'];
+
+//         for (i = 0; i < timestamps.length; i++) {
+//             console.log(timestamps[i] + ": " + closePrices[i]);
+//         }
+
+//         // Create and display chart.
+//         const ctx = document.getElementById("StockChart").getContext("2d");
+
+//         // Destroy old chart if it exists:
+//         var pos = $(document).scrollTop();
+//         if (window.myChart) {
+//             window.myChart.destroy();
+//         }
+//         window.myChart = new Chart(ctx, {
+//             type: "line",
+//             data: {
+//                 labels: timestamps,
+//                 datasets: [{
+//                     label: "Close Prices",
+//                     data: closePrices,
+//                     borderColor: "rgba(75, 192, 192, 1)",
+//                     borderWidth: 1,
+//                     fill: false
+//                 }]
+//             },
+//             options: {
+//                 plugins: {
+//                     title: {
+//                         display: true,
+//                         text: 'Intraday Close Prices:',
+//                         padding: {
+//                             top: 10,
+//                             bottom: 30
+//                         }
+//                     }
+//                 },
+//                 scales: {
+//                     x: {
+//                         type: 'timeseries',
+//                         time: {
+//                             unit: 'hour', // Display time only
+//                             tooltipFormat: 'h:mm a', // Format for tooltips
+//                             displayFormats: {
+//                                 hour: 'h:mm a', // Format for the x-axis labels
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         });
+//         $(document).scrollTop(pos);
+//     } catch (error) {
+//         console.error('Error displaying chart:', error);
+//     }
+// }
+
+//Function to display intraday line graph of stock data:
 async function display_intraday_chart(){
-    try {
-        const chart_data = await getStockData('intraday');
-        //console.log(chart_data)
-        // timestamps = chart_data['timestamps'].map(timestamp => new Date(timestamp));
-        // // Adjust hours to match UTD.
-        // timestamps.forEach((timestamp, index) => {
-        //     const currentHours = timestamp.getHours();
-        //     timestamps[index] = new Date(timestamp.setHours(currentHours + 5));
-        // });
-        timestamps = chart_data['timestamps'];
-        closePrices = chart_data['closePrices'];
-
-        for (i = 0; i < timestamps.length; i++) {
-            console.log(timestamps[i] + ": " + closePrices[i]);
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Intraday Close Prices:',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'hour', // Display time only
+                    tooltipFormat: 'h:mm a', // Format for tooltips
+                    displayFormats: {
+                        hour: 'h:mm a', // Format for the x-axis labels
+                    }
+                }
+            }
         }
+    }
+    const chart = await new create_chart('intraday', options);
+    chart.displayChart();
+}
 
-        // Create and display chart.
-        const ctx = document.getElementById("StockChart").getContext("2d");
-
-        // Destroy old chart if it exists:
-        var pos = $(document).scrollTop();
-        if (window.myChart) {
-            window.myChart.destroy();
-        }
-        window.myChart = new Chart(ctx, {
-            type: "line",
-            data: {
-                labels: timestamps,
-                datasets: [{
-                    label: "Close Prices",
-                    data: closePrices,
-                    borderColor: "rgba(75, 192, 192, 1)",
-                    borderWidth: 1,
-                    fill: false
-                }]
-            },
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Intraday Close Prices:',
-                        padding: {
-                            top: 10,
-                            bottom: 30
-                        }
+async function display_week_chart(){
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: '5-Day Trailing Close Prices:',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'day', // Display time only
+                    tooltipFormat: 'MMM d, h:mm a', // Format for tooltips
+                    displayFormats: {
+                        day: 'MMM d', // Format for the x-axis labels
                     }
                 },
-                scales: {
-                    x: {
-                        type: 'timeseries',
-                        time: {
-                            unit: 'hour', // Display time only
-                            tooltipFormat: 'h:mm a', // Format for tooltips
-                            displayFormats: {
-                                hour: 'h:mm a', // Format for the x-axis labels
-                            },
-                            timezone: 'UTC'
-                        },
-                        title: {
-                            display: true
-                            //text: 'Time',
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true
-                            //text: 'Close Price',
+                ticks: {
+                    // function to fix overlapping labels bug.
+                    callback: function(value) {
+                        // Calculate the day of the week (0 for Sunday, 6 for Saturday)
+                        let dayOfWeek = new Date(Date.parse(value)).getDay();
+                        // Check if it's a weekend (Sunday or Saturday)
+                        if (dayOfWeek === 0 || dayOfWeek === 6) {
+                          // Return undefined to skip the label
+                          return;
+                        } else {
+                          // Return the label to display it
+                          return value;
                         }
                     }
                 }
             }
-        });
-        $(document).scrollTop(pos);
-    } catch (error) {
-        console.error('Error displaying chart:', error);
+        }
     }
+    const chart = await new create_chart('weekly', options);
+    chart.displayChart();
 }
+
+async function display_month_chart(){
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: ' 1 Month Trailing Close Prices:',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'day', // Display time only
+                    tooltipFormat: 'MMM d', // Format for tooltips
+                    displayFormats: {
+                        day: 'MMM d', // Format for the x-axis labels
+                    }
+                },
+                ticks: {
+                    callback: function(value) {
+                        // Calculate the day of the week (0 for Sunday, 6 for Saturday)
+                        let dayOfWeek = new Date(Date.parse(value)).getDay();
+                        // First check if it's a weekend (Sunday or Saturday)
+                        if (dayOfWeek === 0 || dayOfWeek === 6) {
+                            // Return undefined to skip the label (to handle chart.js bug)
+                            return;
+                        } else {
+                            return value;
+                        }
+                    },
+                    maxTicksLimit: 6, // to make x-axis less crowded.
+                    autoSkip: true
+                }
+            }
+        }
+    }
+    const chart = await new create_chart('monthly', options);
+    chart.displayChart();
+}
+
+async function display_ytd_chart(){
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: 'YTD Close Prices:',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'day', // Display time only
+                    tooltipFormat: 'MMM d, y', // Format for tooltips
+                    displayFormats: {
+                        day: 'MMM', // Format for the x-axis labels
+                    }
+                },
+                ticks: {
+                    callback: function(value) {
+                        // Calculate the day of the week (0 for Sunday, 6 for Saturday)
+                        let dayOfWeek = new Date(Date.parse(value)).getDay();
+                        // First check if it's a weekend (Sunday or Saturday)
+                        if (dayOfWeek === 0 || dayOfWeek === 6) {
+                            // Return undefined to skip the label (to handle chart.js bug)
+                            return;
+                        } else {
+                            return value;
+                        }
+                    },
+                    maxTicksLimit: 4, // to make x-axis less crowded.
+                    autoSkip: true
+                }
+            }
+        }
+    }
+    const chart = await new create_chart('ytd', options);
+    chart.displayChart();
+}
+
+async function display_oneYear_chart(){
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: '1 Year Close Prices: ',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'day', // Display time only
+                    tooltipFormat: 'MMM d, y', // Format for tooltips
+                    displayFormats: {
+                        day: 'MMM', // Format for the x-axis labels
+                    }
+                },
+                ticks: {
+                    callback: function(value) {
+                        // Calculate the day of the week (0 for Sunday, 6 for Saturday)
+                        let dayOfWeek = new Date(Date.parse(value)).getDay();
+                        // First check if it's a weekend (Sunday or Saturday)
+                        if (dayOfWeek === 0 || dayOfWeek === 6) {
+                            // Return undefined to skip the label (to handle chart.js bug)
+                            return;
+                        } else {
+                            return value;
+                        }
+                    },
+                    maxTicksLimit: 4, // to make x-axis less crowded.
+                    autoSkip: true
+                }
+            }
+        }
+    }
+    const chart = await new create_chart('year', options);
+    chart.displayChart();
+}
+
+async function display_fiveYear_chart(){
+    var options = {
+        plugins: {
+            title: {
+                display: true,
+                text: '5 Year Close Prices: ',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            }
+        },
+        scales: {
+            x: {
+                type: 'timeseries',
+                time: {
+                    unit: 'day', // Display time only
+                    tooltipFormat: 'MMM d, y', // Format for tooltips
+                    displayFormats: {
+                        day: 'y', // Format for the x-axis labels
+                    }
+                },
+                ticks: {
+                    callback: function(value) {
+                        // Calculate the day of the week (0 for Sunday, 6 for Saturday)
+                        let dayOfWeek = new Date(Date.parse(value)).getDay();
+                        // First check if it's a weekend (Sunday or Saturday)
+                        if (dayOfWeek === 0 || dayOfWeek === 6) {
+                            // Return undefined to skip the label (to handle chart.js bug)
+                            return;
+                        } else {
+                            return value;
+                        }
+                    },
+                    maxTicksLimit: 4, // to make x-axis less crowded.
+                    autoSkip: true
+                }
+            }
+        }
+    }
+    const chart = await new create_chart('5year', options);
+    chart.displayChart();
+}
+   
+
 
 // Handle button clicks
 // 1 Day Button:
 document.getElementById('btn-1day').addEventListener('click', function () {
     display_intraday_chart();
 });
-// // 5 Days Button:
-// document.getElementById('btn-5days').addEventListener('click', function () {
-//     display_week_chart(tickerSymbol);
-// });
-// // 1 Month Button:
-// document.getElementById('btn-1month').addEventListener('click', function () {
-//     display_month_chart(tickerSymbol);
-// });
-// // YTD Button:
-// document.getElementById('btn-ytd').addEventListener('click', function () {
-//     display_ytd_chart(tickerSymbol);
-// });
-// // 1 Year Button:
-// document.getElementById('btn-1year').addEventListener('click', function () {
-//     display_oneYear_chart(tickerSymbol);
-// });
-// // 5 Year Button:
-// document.getElementById('btn-5year').addEventListener('click', function () {
-//     display_fiveYear_chart(tickerSymbol);
-// });
+// 5 Days Button:
+document.getElementById('btn-5days').addEventListener('click', function () {
+    display_week_chart();
+});
+// 1 Month Button:
+document.getElementById('btn-1month').addEventListener('click', function () {
+    display_month_chart();
+});
+// YTD Button:
+document.getElementById('btn-ytd').addEventListener('click', function () {
+    display_ytd_chart();
+});
+// 1 Year Button:
+document.getElementById('btn-1year').addEventListener('click', function () {
+    display_oneYear_chart();
+});
+// 5 Year Button:
+document.getElementById('btn-5year').addEventListener('click', function () {
+    display_fiveYear_chart();
+});
 // // All Time Button:
 // document.getElementById('btn-allTime').addEventListener('click', function () {
 //     display_allTime_chart(tickerSymbol);
