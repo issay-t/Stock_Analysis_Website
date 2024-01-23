9bad55c2d43484eeddd763e1c17213485e451937
Issay Tieu <issaytieu@Issays-MacBook-Pro.local>
2024-01-23 10:25:28 -0500

    Added local database feature to increase API call efficiency using pickle module.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from process_data import *
+from datetime import datetime
 import os
+import pickle
+
 
 app = Flask(__name__)
 
@@ -25,21 +28,47 @@ def send_data(lengthTime):
     global ticker_symbol  # Access the global ticker_symbol set previously
     global local_data
 
-    # Call the backend function to get new data based on the ticker symbol
+    # Specify the folder to store the files
+    folder_path = 'local_database/'
+    filepath = os.path.join(folder_path, f'{ticker_symbol}_{lengthTime}.pkl')
+
+    # Check if the file exists and is older than a day
+    if (os.path.exists(filepath)):
+        file_modified_time = datetime.fromtimestamp(os.path.getmtime(filepath))
+        current_time = datetime.now()
+
+        # If the file was uploaded in the same day, use it.
+        if (current_time.date() == file_modified_time.date()):
+            with open(filepath, 'rb') as file:
+                cached_data = pickle.load(file)
+                return jsonify(cached_data)
+        else:
+            os.remove(filepath)
+            
+    # Else the file does not exist. Request new information.
+    # Call the backend function to get new data based on the ticker symbol 
+    # and save information into a file.
+    stock_info = None
     if (lengthTime == "intraday") :
-        return jsonify(getIntradayData(ticker_symbol))
+        stock_info = getIntradayData(ticker_symbol)
     elif (lengthTime == "weekly") :
-        return jsonify(getWeeklyData(ticker_symbol))
+        stock_info = getWeeklyData(ticker_symbol)
     elif (lengthTime == "monthly") :
-        return jsonify(getMonthlyData(ticker_symbol))
+        stock_info = getMonthlyData(ticker_symbol)
     elif (lengthTime == "ytd") :
-        return jsonify(getYTDData(ticker_symbol))
+        stock_info = getYTDData(ticker_symbol)
     elif (lengthTime == "year") :
-        return jsonify(getOneYearData(ticker_symbol))
+        stock_info = getOneYearData(ticker_symbol)
     elif (lengthTime == "5year") :
-        return jsonify(getFiveYearData(ticker_symbol))
+        stock_info = getFiveYearData(ticker_symbol)
     else : 
-        return jsonify(getAllTimeData(ticker_symbol))
+        stock_info = getAllTimeData(ticker_symbol)
+
+    # Save the extracted python data to a pickle file in the local database.
+    with open(filepath, 'wb') as file:
+        pickle.dump(stock_info, file)
+
+    return jsonify(stock_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
